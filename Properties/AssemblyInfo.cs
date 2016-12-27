26d40cf08d3d025de9eeb594b5661b71f033a97b
Allis Tauri <allista@gmail.com>
2016-12-27 19:37:42 +0300

    Changed version to 2.4.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.0.1")]
 [assembly: KSPAssembly("ConfigurableContainers", 2, 3)]
 
 // The following attributes are used to specify the signing key for the assembly,
