e8c8570ab7c77241b055247b09e1c82aaa9cd9f7
Allis Tauri <allista@gmail.com>
2017-06-17 01:10:10 +0300

    Changed version to 2.4.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Reflection;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("2.4.*")]
 #else
-[assembly: AssemblyVersion("2.4.0.6")]
+[assembly: AssemblyVersion("2.4.1.0")]
 #endif
 [assembly: KSPAssembly("ConfigurableContainers", 2, 3)]
 
