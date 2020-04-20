4c98ed5382d92ff5b8d9820dbe79e2ac7d1954c9
Evgeny Taranov <taranov@jet.su>
2020-04-20 22:18:24 +0300

    AssemblyVersion: 2.4.8.1
    Recompiled with new AT_Utils

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Reflection;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("2.4.*")]
 #else
-[assembly: AssemblyVersion("2.4.8")]
+[assembly: AssemblyVersion("2.4.8.1")]
 #endif
 [assembly: KSPAssembly("ConfigurableContainers", 2, 4)]
 
