c817473637a1a172bb0b6c6afba004fb9b686d2c
Allis Tauri <allista@gmail.com>
2020-06-22 20:22:05 +0300

    AssemblyVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -25,11 +25,11 @@ using System.Reflection;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 #if NIGHTBUILD
-[assembly: AssemblyVersion("2.4.*")]
+[assembly: AssemblyVersion("2.5.*")]
 #else
-[assembly: AssemblyVersion("2.4.8.3")]
+[assembly: AssemblyVersion("2.5.0")]
 #endif
-[assembly: KSPAssembly("ConfigurableContainers", 2, 4)]
+[assembly: KSPAssembly("ConfigurableContainers", 2, 5)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
