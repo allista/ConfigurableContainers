d3758732f5ae58b8efb093b69ff0ea54774f2160
Allis Tauri <allista@gmail.com>
2022-02-03 11:49:03 +0300

    AssemblyVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Reflection;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("2.6.*")]
 #else
-[assembly: AssemblyVersion("2.6.1")]
+[assembly: AssemblyVersion("2.6.2")]
 #endif
 [assembly: KSPAssembly("ConfigurableContainers", 2, 6)]
 
